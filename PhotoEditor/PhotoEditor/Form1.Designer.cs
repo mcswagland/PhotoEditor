fcd9ebc813c7f88ab78ccf9f718efb2b16716fc2
brck4christ <bmcfarland@SI-201-13.harding.edu>
2015-09-15 21:00:12 -0500

    Initial Commit
    Work on displaying directory view

---
+++ PhotoEditor/PhotoEditor/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PhotoEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryView = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.directoryWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // directoryView
+            // 
+            this.directoryView.Location = new System.Drawing.Point(12, 12);
+            this.directoryView.Name = "directoryView";
+            this.directoryView.Size = new System.Drawing.Size(200, 425);
+            this.directoryView.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(217, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(572, 424);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // directoryWorker
+            // 
+            this.directoryWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.directoryWorker_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 449);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.directoryView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView directoryView;
+        private System.Windows.Forms.ListView listView1;
+        private System.ComponentModel.BackgroundWorker directoryWorker;
+    }
+}
+
---
+++ PhotoEditor/PhotoEditor/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PhotoEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void form1_addDirectory(string dir)
+        {
+
+        }
+
+        private void directoryWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //public delegate void addDirectoryDelegate(string dirName);
+
+            //public event addDirectoryDelegate addDirectory;
+
+            //addDirectory += form1_addDirectory;
+            // change this to a variable probably holding the root directory
+            DirectoryInfo homeDir = new DirectoryInfo(@"M:\");
+            GetDirectories(homeDir);
+            /*
+            List<DirectoryInfo> directories = homeDir.GetDirectories().ToList();
+            directories.Add(homeDir);
+            //directories.
+            foreach (DirectoryInfo dir in directories)
+            {
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    try
+                    {
+                        if (file.Extension.ToLower() == ".jpg")
+                        {
+                            byte[] bytes = System.IO.File.ReadAllBytes(file.FullName);
+                            MemoryStream ms = new MemoryStream(bytes);
+                            Image img = Image.FromStream(ms);
+                            Console.WriteLine("Filename: " + file.Name);
+                            Console.WriteLine("Last mod: " + file.LastWriteTime.ToString());
+                            Console.WriteLine("File size: " + file.Length);
+
+                            directoryView.Nodes.Add("");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("This is not an image file");
+                    }
+                }
+            }
+             * */
+        }
+
+        private void GetDirectories(DirectoryInfo info)
+        {
+            foreach(DirectoryInfo dir in info.GetDirectories())
+            {
+                if(info.Root.ToString() == info.Name)
+                {
+                    directoryView.Nodes.Add(info.Name);
+                }
+                else
+                {
+                    directoryView.Nodes.Add(info.Name, dir.Name);
+                }
+                GetDirectories(dir);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!directoryWorker.IsBusy)
+                directoryWorker.RunWorkerAsync();  
+        }
+    }
+}
